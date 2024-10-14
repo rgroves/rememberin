6c1d087a14d15d281702ff5e17b8945ecc457279
Robert Groves <rwgdev@gmail.com>
2024-10-13 22:49:36 -0500

    Add env var config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 import react from '@astrojs/react';
 
@@ -7,10 +7,20 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  output: "hybrid",
-
   adapter: node({
     mode: 'standalone'
-  })
+  }),
+  experimental: {
+    env: {
+      schema: {
+        AWS_ACCESS_KEY_ID: envField.string({ context: "server", access: "secret" }),
+        AWS_ENDPOINT_URL_S3: envField.string({ context: "server", access: "secret" }),
+        AWS_REGION: envField.string({ context: "server", access: "secret" }),
+        AWS_SECRET_ACCESS_KEY: envField.string({ context: "server", access: "secret" }),
+        BUCKET_NAME: envField.string({ context: "server", access: "secret" }),
+      }
+    }
+  },
+  integrations: [react()],
+  output: "hybrid",
 });
--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,12 @@
 import { defineAction } from "astro:actions";
+import { getSecret } from "astro:env/server";
 import { z } from "astro:schema";
+
+const AWS_ACCESS_KEY_ID = getSecret("AWS_ACCESS_KEY_ID");
+const AWS_ENDPOINT_URL_S3 = getSecret("AWS_ENDPOINT_URL_S3");
+const AWS_REGION = getSecret("AWS_REGION");
+const AWS_SECRET_ACCESS_KEY = getSecret("AWS_SECRET_ACCESS_KEY");
+const BUCKET_NAME = getSecret("BUCKET_NAME");
 
 export const server = {
   notes: {
